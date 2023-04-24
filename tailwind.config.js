04c1f864edefcc989bac5facf73ebe4394990eb3
Yoav Ganbar <yoav@builder.io>
2023-04-24 15:46:16 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+//** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  plugins: [],
+};
